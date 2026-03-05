164e9053ffafb45624e37f6b190bafc0839f15e3
Arun Pandi T <162444940+Arunpandikgisl@users.noreply.github.com>
2026-03-05 08:58:47 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,307 @@
+//Section 1
+//1. Addition, Subtraction, Multiplication, Division of 25 and 4
+console.log(25 + 4);
+console.log(25 - 4);
+console.log(25 * 4);
+console.log(25 / 4);
+
+//2. 100 % 30
+console.log(100 % 30);
+
+//3. 7 ** 2
+console.log(7 ** 2);
+
+//4. 2 ** 5
+console.log(2 ** 5);    
+
+//5. Store 50 in a variable and divided by 5
+let arun=50;
+console.log(arun/5);
+
+//6.Reminder of 45 divided by 6
+console.log(45 % 6);
+
+//7.Multiply 0.5 * 8
+console.log(0.5 * 8);
+
+//8.Subtract 200 from 75
+console.log(75 - 200);
+
+//9.Add three numbers (10,20,30)
+console.log(10+20+30);
+
+//10.Find 9 % 2
+console.log(9 % 2);
+
+//Section 2 - Increment / Decrement
+//11.
+let a = 5;
+console.log(a++);
+console.log(a);
+
+//12.
+let a1 = 5;
+console.log(++a1);
+console.log(a1);
+
+//13.
+let a2 = 10;
+let b = a2++;
+console.log(a2,b);
+
+//14.
+let a3 = 10;
+let b1 = ++a3;
+console.log(a3,b1);
+
+//15.
+let a4 =1;
+let b2 = a4++ + ++a4;
+console.log(a4, b2);
+
+//16.
+let a5 =1;
+let b3 =++a5 + ++a5;
+console.log(a5,b3);
+
+//17.
+let a6 =3;
+a6 = a6++;
+console.log(a6);
+
+//18.
+let a7 =3;
+a7 = ++a7;
+console.log(a7);
+
+//19.
+let x =5;
+let y =--x;
+console.log(x, y);
+
+//20.
+let x1 =5;
+let y1 =x1--;
+console.log(x1,y1);
+
+//21.
+let c =2;
+let d =c++ + ++c + c++;
+console.log(c,d);
+
+//22.
+let e =4;
+let f =--e + e-- + --e;
+console.log(e,f);
+
+//23.
+let g =1;
+let h =++g + g++ + ++g;
+console.log(g,h);
+
+//24.
+let i =5;
+let j =i++ + i++ + i++;
+console.log(i,j);
+
+//25.
+let k =10;
+k += k++;
+console.log(k);
+
+//Section 3
+//26.
+let l =10;
+l += 5;
+console.log(l);
+
+//27.
+let m =20;
+m -=8;
+console.log(m);
+
+//28.
+let n =7;
+n *=3;
+console.log(n);
+
+//29.
+let o =40;
+o /=5;
+console.log(o);
+
+//30.
+let p =25;
+p %=4;
+console.log(p);
+
+//31.
+let q =5;
+q +=3;
+q *=2;
+console.log(q);
+
+//32.
+let r =10;
+r -=2;
+r /=4;
+console.log(r);
+
+//33.
+let s =8;
+s %=3;
+console.log(s);
+
+//34.
+let t =6;
+t += t;
+console.log(t);
+
+//35.
+let u =2;
+u *= u += 3;
+console.log(u);
+
+//Section - 4
+//36.
+console.log(5 == "5");
+
+//37.
+console.log(5 === "5");
+
+//38.
+console.log(0 == false);
+
+//39.
+console.log(0 === false);
+
+//40.
+console.log(null == undefined);
+
+//41.
+console.log(null === undefined);
+
+//42.
+console.log("10" > 5);
+
+//43.
+console.log("2" > "10");
+
+//44.
+console.log(true == 1);
+
+//45.
+console.log(true === 1);
+
+//section - 5
+//46.
+console.log(true && false);
+
+//47.
+console.log(true || false);
+
+//48.
+console.log(false || false);
+
+//49.
+console.log(!true);
+
+//50.
+console.log(!(5 > 2));
+
+//51.
+console.log(5 > 3 && 10 > 5);
+
+//52.
+console.log(5 > 3 && 10 < 5);
+
+//53.
+console.log(5 < 3 || 10 < 5);
+
+//54.
+console.log(5 > 3 || 10 < 5);
+
+//55.
+console.log((5 > 2 && 3 > 1)||(10 < 2));
+
+//Section - 6
+//56.
+let age= 20;
+console.log(age >= 18 ? "Adult" : "Minor");
+
+//57.
+let marks = 35;
+console.log(marks >=50 ? "Pass" : "Fail");
+
+//58.
+let number = 8;
+console.log(number % 2 === 0 ? "Even" : "Odd");
+
+//59.
+let Scores = 75;
+let grade = Scores >= 90 ? "A" : 
+            Scores >=75 ? "B" : 
+            Scores >=60 ? "C" : 
+            "Fail";
+console.log(grade);
+
+//60.
+let num = -3;
+console.log(num >= 0 ? "Positive" : "Negative");
+
+//Section - 7
+//61.
+let result = "Hello" + " " + "World";
+console.log(result);
+
+//62.
+let firstname = "Arun";
+let secondname = "Pandi";
+console.log(firstname + " " + secondname);
+
+//63.
+let Firstname = "Arun";
+let Secondname = "Pandi";        
+console.log(`${Firstname} ${Secondname}`);
+
+//64.
+console.log("10" + 5);
+
+//65.
+console.log(5 + "10");
+
+//Section 8
+//66.
+console.log("5"+2);
+
+//67.
+console.log("5"-2);
+
+//68.
+console.log("5"*2);
+
+//69.
+console.log(true + 1);
+
+//70.
+console.log(false + "1");
+
+//section 9
+//71.
+console.log(Number("123"));
+
+//72.
+console.log(Number("abc"));
+
+//73.
+console.log(Number(true));
+
+//74.
+console.log(Boolean(""));
+
+//75.
+console.log(Boolean("hello"))
+
+
+
+
